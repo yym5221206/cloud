3ada05d2432cc7271ed014cb33f40990cfbc92f3
yanyimin <18670473318@163.com>
2017-05-11 17:48:04 +0800

    将config-server版本改为1.2.1后修复在git上进行配置

---
+++ cloud-config-mapped/src/main/java/com/bees/cloud/core/ConfigMappedApp.java
@@ -0,0 +1,12 @@
+package com.bees.cloud.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigMappedApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigMappedApp.class, args);
+	}
+}
